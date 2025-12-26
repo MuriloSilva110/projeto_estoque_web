41e0564d3fe66764948409d4f8650132e4285047
Murilo Silva <murilo.gomes12339@gmail.com>
2025-12-26 19:10:24 -0300

    Remove criação automática das tabelas no final da aplicação

--- app.py
+++ app.py
@@ -301,7 +301,4 @@ def logout():
     return redirect(url_for('home'))
 # Inicializa o app
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
